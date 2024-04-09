22f3ca6c3599d4ed9e434664a26ff6f391547a13
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-04-09 17:59:33 -0400

    Start on login reg forms

---
+++ client/src/Components/LoginRegForms/LoginForm.jsx
@@ -0,0 +1,17 @@
+import "./LoginReg.css"
+
+function LoginForm() {
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <div className="card bg-dark">
+                <div className="card-body">
+                    <h5 className="card-title">form</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm
---
+++ client/src/Components/LoginRegForms/RegisterForm.jsx
@@ -0,0 +1,12 @@
+import "./LoginReg.css"
+
+function LoginForm() {
+
+    return (
+        <div>
+            <h2>Register</h2>
+        </div>
+    )
+}
+
+export default LoginForm
