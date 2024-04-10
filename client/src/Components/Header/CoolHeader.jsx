b895530c86ca36171dbdd7b1e88622f5bca9353a
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-04-10 17:58:40 -0400

    Create the cool header component and styling

---
+++ client/src/Components/Header/CoolHeader.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './CoolHeader.css';
+
+const CoolHeader = () => {
+    return (
+        <h1 className="cool-header">
+            <span>W</span>
+            <span>e</span>
+            <span>l</span>
+            <span>c</span>
+            <span>o</span>
+            <span>m</span>
+            <span>e</span>
+            <span>&nbsp;</span> {/* Add space */}
+            <span>T</span>
+            <span>o</span>
+            <span>&nbsp;</span> {/* Add space */}
+            <span>G</span>
+            <span>a</span>
+            <span>m</span>
+            <span>e</span>
+            <span>B</span>
+            <span>r</span>
+            <span>i</span>
+            <span>d</span>
+            <span>g</span>
+            <span>e</span>
+        </h1>
+    );
+};
+
+export default CoolHeader;
