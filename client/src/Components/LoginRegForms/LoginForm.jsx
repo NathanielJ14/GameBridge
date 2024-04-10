7b7b6ae3d2ee2da80768c92b18361c341cb7fee2
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-04-10 17:58:57 -0400

    Set up styling for login reg forms

--- client/src/Components/LoginRegForms/LoginForm.jsx
+++ client/src/Components/LoginRegForms/LoginForm.jsx
@@ -3,11 +3,21 @@ import "./LoginReg.css"
 function LoginForm() {
 
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="form">
             <div className="card bg-dark">
+                <h2 className="text-center mt-2">Login</h2>
                 <div className="card-body">
-                    <h5 className="card-title">form</h5>
+                    <form>
+                        <div className="mb-3">
+                            <input type="email" className="form-control" id="email" required placeholder="Email" />
+                        </div>
+                        <div className="mb-3">
+                            <input type="password" className="form-control" id="password" required placeholder="Password" />
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <button type="submit" className="btn mt-2">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
--- client/src/Components/LoginRegForms/RegisterForm.jsx
+++ client/src/Components/LoginRegForms/RegisterForm.jsx
@@ -3,8 +3,29 @@ import "./LoginReg.css"
 function LoginForm() {
 
     return (
-        <div>
-            <h2>Register</h2>
+        <div className="form">
+            <div className="card bg-dark">
+                <h2 className="text-center mt-2">Register</h2>
+                <div className="card-body">
+                    <form>
+                        <div className="mb-3">
+                            <input type="text" className="form-control" id="username" required placeholder="Username" />
+                        </div>
+                        <div className="mb-3">
+                            <input type="email" className="form-control" id="email" required placeholder="Email" />
+                        </div>
+                        <div className="mb-3">
+                            <input type="password" className="form-control" id="password" required placeholder="Password" />
+                        </div>
+                        <div className="mb-3">
+                            <input type="password" className="form-control" id="confirmPassword" required placeholder="Confirm Password" />
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <button type="submit" className="btn mt-2">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
