b48a355133bd220d1bd03fc88a18ed39169608c5
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-04-16 19:00:12 -0400

    Restructure backend

--- server/server.js
+++ server/server.js
@@ -1,129 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
+const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 3001;
-
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    methods: ['POST', 'GET'],
+    credentials: true
+}));
 app.use(cookieParser());
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false,
-        maxAge: 1000 * 60 * 60 * 24
-    }
-}))
 
-// Connect to SQLite database
-const db = new sqlite3.Database('./gamebridgeDB.db', (err) => {
-    if (err) {
-        console.error('Error connecting to SQLite database:', err);
-    } else {
-        console.log('Connected to SQLite database');
-        // Create users table
-        db.run(
-            `CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
-            )`
-        );
-    }
-});
-
-// API endpoint for user registration
-app.post('/api/register', async (req, res) => {
-    const { username, email, password } = req.body;
-
-    try {
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Insert user into database
-        const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-        db.run(sql, [username, email, hashedPassword], (err) => {
-            if (err) {
-                console.error('Error registering user:', err);
-                res.status(500).json({ error: 'Registration failed' });
-            } else {
-                console.log('User registered successfully');
-                res.json({ message: 'Registration successful' });
-                // Save user info in session
-                req.session = {
-                    username: username, email: email
-                };
-            }
-        });
-    } catch (error) {
-        console.error('Error registering user:', error);
-        res.status(500).json({ error: 'Registration failed' });
-    }
-});
+//Create new mysql db connection
 
 
-// API endpoint for user login
-app.post('/api/login', async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        // Check if the user exists in the database
-        const sql = 'SELECT * FROM users WHERE email = ?';
-        db.get(sql, [email], async (err, user) => {
-            if (err) {
-                console.error('Error finding user:', err);
-                return res.status(500).json({ error: 'Login failed' });
-            }
-
-            if (!user) {
-                return res.status(401).json({ error: 'Invalid credentials' });
-            }
-
-            // Compare the provided password with the hashed password stored in the database
-            const isPasswordMatch = await bcrypt.compare(password, user.password);
-            if (!isPasswordMatch) {
-                return res.status(401).json({ error: 'Invalid credentials' });
-            }
-
-            res.json({ message: 'Login successful', user: { id: user.id, username: user.username, email: user.email } });
-            // Save user info in session
-            req.session = {
-                id: user.id, username: user.username, email: user.email
-            };
-
-        });
-    } catch (error) {
-        console.error('Error logging in user:', error);
-        res.status(500).json({ error: 'Login failed' });
-    }
-});
-
-
-// API endpoint for user logout
-app.post('/api/logout', (req, res) => {
-    try {
-        // Destroy the session
-        req.session.destroy((err) => {
-            if (err) {
-                console.error('Error destroying session:', err);
-                return res.status(500).json({ error: 'Logout failed' });
-            }
-            // Session destroyed successfully
-            res.json({ message: 'Logout successful' });
-        });
-    } catch (error) {
-        console.error('Logout failed:', error);
-        res.status(500).json({ error: 'Logout failed' });
-    }
-});
 
 
 
