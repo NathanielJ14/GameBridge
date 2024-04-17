248fa484be9623dd7a78ed051cf6d24399fdb0f6
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-04-17 18:09:34 -0400

    Set up register function

--- client/src/Components/LoginRegForms/RegisterForm.jsx
+++ client/src/Components/LoginRegForms/RegisterForm.jsx
@@ -4,73 +4,38 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const RegisterForm = () => {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+    const [values, setValues] = useState({
+        userName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        axios.post('http://localhost:3001/api/register', username, email, password, confirmPassword)
-            .then(res => {
-                if (res.data.Status === 'Success') {
-                    navigate('/dashboard');
-                } else {
-                    alert('Error');
-                }
-            })
+        axios.post('http://localhost:3001/register', values)
+            .then(res => console.log(res))
             .then(err => console.log(err));
     }
-
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-
-    //     // Check if passwords match
-    //     if (password !== confirmPassword) {
-    //         setError('Passwords do not match');
-    //         return;
-    //     }
-
-    //     try {
-    //         // Send registration data to backend
-    //         const response = await fetch('http://localhost:3001/api/register', {
-    //             method: 'POST',
-    //             headers: { 'Content-Type': 'application/json' },
-    //             body: JSON.stringify({ username, email, password }),
-    //         });
-
-    //         const data = await response.json();
-    //         console.log(data); // Handle success response
-    //         navigate('/dashboard');
-
-    //     } catch (error) {
-    //         setError('Registration failed. Please try again.');
-    //         console.error('Error:', error);
-    //     }
-    // };
-
 
     return (
         <div className="form">
             <div className="card bg-dark">
                 <h2 className="text-center mt-2">Register</h2>
                 <div className="card-body">
-                    {/* {error && <p style={{ color: 'red' }}>{error}</p>} */}
                     <form onSubmit={handleSubmit}>
                         <div className="mb-3">
-                            <input type="text" className="form-control" name="username" value={username} required placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+                            <input type="text" className="form-control" name="userName" required placeholder="Username" onChange={e => setValues({ ...values, userName: e.target.value })} />
                         </div>
                         <div className="mb-3">
-                            <input type="email" className="form-control" name="email" value={email} required placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                            <input type="email" className="form-control" name="email" required placeholder="Email" onChange={e => setValues({ ...values, email: e.target.value })} />
                         </div>
                         <div className="mb-3">
-                            <input type="password" className="form-control" name="password" value={password} required placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                            <input type="password" className="form-control" name="password" required placeholder="Password" onChange={e => setValues({ ...values, password: e.target.value })} />
                         </div>
                         <div className="mb-3">
-                            <input type="password" className="form-control" name="confirmPassword" value={confirmPassword} required placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} />
+                            <input type="password" className="form-control" name="confirmPassword" required placeholder="Confirm Password" onChange={e => setValues({ ...values, confirmPassword: e.target.value })} />
                         </div>
                         <div className="d-flex justify-content-center">
                             <button type="submit" className="btn mt-2">Register</button>
--- server/server.js
+++ server/server.js
@@ -1,24 +1,51 @@
+require('dotenv').config();
+
 const express = require('express');
-const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
-const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const salt = 10;
 
 const app = express();
 const port = 3001;
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    methods: ['POST', 'GET'],
-    credentials: true
-}));
+app.use(cors({}));
 app.use(cookieParser());
 
-//Create new mysql db connection
+// Connect to the MySQL Database
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
 
+// // Register User
+app.post('/register', (req, res) => {
+    const sql = 'INSERT INTO users (`userName`, `email`, `password`) VALUES (?)';
+    const { userName, email, password } = req.body;
+
+    bcrypt.hash(password.toString(), salt, (err, hash) => {
+        if (err) {
+            console.error('Error hashing password:', err);
+            return res.status(500).json({ Error: 'Error hashing password' });
+        }
+
+        const values = [userName, email, hash];
+
+        db.query(sql, [values], (err, result) => {
+            if (err) {
+                console.error('Error inserting data:', err);
+                return res.status(500).json({ Error: 'Error inserting data' });
+            }
+
+            console.log('User registered successfully:', result.insertId);
+            return res.status(200).json({ Status: 'Success' });
+        });
+    });
+});
 
 
 
