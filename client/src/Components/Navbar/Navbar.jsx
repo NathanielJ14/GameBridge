7799d849f1348b19caab4d7ac8759ae823a444c7
NathanielJ14 <Nathanielalbert1202@gmail.com>
2024-04-23 13:36:38 -0400

    Updating navbar and style

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand navHeader" href="/dashboard">GameBridge</a>
+                <h1 className="navbar-brand navHeader my-0">GameBridge</h1>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        <a className="nav-link" href="#">Features</a>
+                        <a className="nav-link active" href="/dashboard">Dashboard</a>
+                        <a className="nav-link" href="/account">Link Accounts</a>
                     </div>
                     <div className="">
                         <Logout />
